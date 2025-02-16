c8ce067f337ba1245a89310729e39091d1f2790a
sdeavin22 <stephen.schaer@gmail.com>
2025-02-16 13:13:55 -0800

    in process dark/light mode switch

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,16 +1,10 @@
 import Link from 'next/link';
-// import DarkModeSwitch from './DarkModeSwitch';
-// import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import DarkModeSwitch from '@/app/components/DarkModeSwitch';
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
       <ul className='flex gap-4'>
-        {/* <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedOut> */}
           <Link href={'/sign-in'}>Sign in</Link>
-        {/* </SignedOut> */}
         <li className='hidden sm:block'>
           <Link href={'/'}>Home</Link>
         </li>
@@ -23,7 +17,7 @@ export default function Header() {
       </ul>
 
       <div className='flex items-center gap-4'>
-        {/* <DarkModeSwitch /> */}
+        <DarkModeSwitch />
         <Link href={'/'} className='flex gap-1 items-center'>
           <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
             IMDb
